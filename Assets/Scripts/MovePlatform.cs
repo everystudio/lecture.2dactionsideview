6811a7d4feae64885ead541961536652cdfbbc18
watanabe@marida <watanabe.everystudio@gmail.com>
2022-05-13 11:24:00 +0900

    DoTweenを使って動く床を作成

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovePlatform : MonoBehaviour
+{
+    [SerializeField] private float distance = 5f;
+    private float speed = 1f;
+
+    void Start()
+    {
+        float duration = distance / speed;
+        transform.DOMoveX(distance, duration)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetRelative(true)
+            .SetEase(Ease.InOutQuad);
+    }
+
+}
