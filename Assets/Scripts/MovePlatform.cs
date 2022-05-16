53a0d1a6de85bfaa02a8401bf330d5437cae29dc
watanabe@marida <watanabe.everystudio@gmail.com>
2022-05-16 14:52:32 +0900

    床を動かす処理 録画で作ったやつ

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovePlatform : MonoBehaviour
+{
+    public float move = 5f;
+    public float speed = 1f;
+    void Start()
+    {
+        float dulation = move / speed;
+        transform.DOMoveX(move, dulation)
+            .SetRelative(true)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutQuad);
+    }
+}
