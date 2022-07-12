13624c433b0b5c7c81ac3cc6dba7d0737f99387c
watanabe@marida <watanabe.everystudio@gmail.com>
2022-07-12 21:24:08 +0900

    UIでライフを表示する

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     public int hp_current;
     public int hp_max;
+    public UnityEvent<int> OnHealthChange = new UnityEvent<int>();
+    private void Start()
+    {
+        OnHealthChange.Invoke(hp_current);
+    }
 
     public void Damage(int _iParam)
     {
         hp_current -= Mathf.Abs(_iParam);
+        OnHealthChange.Invoke(hp_current);
         PlayerController player = GetComponent<PlayerController>();
         if (player != null)
         {
---
+++ Assets/Scripts/HealthHolder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHolder : MonoBehaviour
+{
+    public Image[] m_imageArr;
+    public int m_iHealth;
+
+    public void SetHealth(int _iHealth)
+    {
+        m_iHealth = _iHealth;
+        for (int i = 0; i < m_imageArr.Length; i++)
+        {
+            m_imageArr[i].enabled = i < m_iHealth;
+        }
+    }
+
+}
