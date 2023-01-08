9aa72162d6a6a8b87bf90dde5c2772f3b87b1fe4
watanabe@marida <watanabe.everystudio@gmail.com>
2023-01-08 11:05:11 +0900

    スマートコンソールを追加

---
+++ Assets/Scripts/WarpArea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpArea : MonoBehaviour
+{
+    [SerializeField] private Transform targetPositionTransform;
+    [SerializeField] private Transform popVFXPrefab;
+
+    public Vector3 GetTargetPosition()
+    {
+        return targetPositionTransform.position;
+    }
+
+    public void VanishPlayer(GameObject targetGameObject, float dulation)
+    {
+        StartCoroutine(SpawnDelay(targetGameObject, dulation));
+    }
+
+    IEnumerator SpawnDelay(GameObject targetGameObject, float delayTime)
+    {
+        targetGameObject.SetActive(false);
+        yield return new WaitForSecondsRealtime(delayTime);
+        targetGameObject.SetActive(true);
+        if (targetGameObject.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody))
+        {
+            rigidbody.velocity = Vector2.up * 2f;
+        }
+        Instantiate(popVFXPrefab, targetGameObject.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/WarpPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPlayer : MonoBehaviour
+{
+    [SerializeField] private float popSpeed = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Warp"))
+        {
+            if (collision.gameObject.TryGetComponent<WarpArea>(out WarpArea warpArea))
+            {
+                Vector3 targetPosition = warpArea.GetTargetPosition();
+                transform.position = targetPosition;
+                if (transform.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody))
+                {
+                    rigidbody.velocity = Vector2.up * popSpeed;
+                }
+
+                warpArea.VanishPlayer(gameObject, 0.5f);
+            }
+        }
+    }
+
+}
